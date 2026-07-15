541988932129874e60360c5d8f69a1349598d02d
fm-dev <fm1441h@gmail.com>
2026-07-15 16:26:05 +0700

    fixing image

--- src/components/Galery.tsx
+++ src/components/Galery.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import '../index.css';
 
 import LightGallery from 'lightgallery/react';
@@ -9,23 +9,70 @@ import 'lightgallery/css/lg-thumbnail.css';
 
 import lgZoom from 'lightgallery/plugins/zoom';
 import lgVideo from 'lightgallery/plugins/video';
-// import lgThumbnail from 'lightgallery/plugins/thumbnail';
 import fjGallery from 'flickr-justified-gallery';
+
+// 1. Definisikan list gambar di luar komponen agar tidak di-render ulang
+const IMAGES = [
+  "https://wedding-fajri.s3.nevaobjects.id/nrimursjqvbyvbuzvsji.webp",
+  "https://wedding-fajri.s3.nevaobjects.id/b6hgvbszjfghbwybfwpg.webp",
+  "https://wedding-fajri.s3.nevaobjects.id/q0oezvlxhthdlgpyhzpy.webp",
+  "https://wedding-fajri.s3.nevaobjects.id/z16oluuzkmc3mwtqshco.webp",
+  "https://wedding-fajri.s3.nevaobjects.id/c1uv7cddikrv96eykn1v.webp",
+  "https://wedding-fajri.s3.nevaobjects.id/eisztlp8fjvyxtzgpnyu.webp",
+  "https://wedding-fajri.s3.nevaobjects.id/hnjyhriidm2tx4rlga5s.webp",
+  "https://wedding-fajri.s3.nevaobjects.id/n3zfkaee9tlbcva0pchh.webp"
+];
+
 export const Gallery = () => {
+  const [imagesLoaded, setImagesLoaded] = useState(false);
+
   useEffect(() => {
-    fjGallery(document.querySelectorAll('.gallery'), {
-      itemSelector: '.gallery__item',
-      rowHeight: 180,
-      lastRow: 'start',
-      gutter: 2,
-      rowHeightTolerance: 0.1,
-      calculateItemsHeight: false,
-    });
+    // 2. Preload semua gambar menggunakan Promise.all
+    const loadImage = (src:any) => {
+      return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.src = src;
+        img.onload = () => resolve(src);
+        img.onerror = (err) => reject(err);
+      });
+    };
+
+    Promise.all(IMAGES.map((image) => loadImage(image)))
+      .then(() => {
+        setImagesLoaded(true);
+      })
+      .catch((err) => {
+        console.error("Gagal memuat beberapa gambar", err);
+        // Tetap set true agar gallery tidak stuck loading jika ada 1 gambar rusak
+        setImagesLoaded(true); 
+      });
   }, []);
+
+  // 3. Inisialisasi Justified Gallery hanya setelah gambar selesai di-preload & di-render
+  useEffect(() => {
+    if (imagesLoaded) {
+      fjGallery(document.querySelectorAll('.gallery'), {
+        itemSelector: '.gallery__item',
+        rowHeight: 180,
+        gutter: 2,
+        rowHeightTolerance: 0.1,
+        calculateItemsHeight: false,
+      });
+    }
+  }, [imagesLoaded]);
+
+  // Tampilkan loading screen sementara gambar diunduh di background
+  if (!imagesLoaded) {
+    return (
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '300px' }}>
+        <p>Memuat Galeri...</p> 
+        {/* Kamu bisa ganti ini dengan spinner CSS buatanmu */}
+      </div>
+    );
+  }
 
   return (
     <div>
-
       <LightGallery
         plugins={[lgZoom, lgVideo]}
         mode="lg-fade"
@@ -43,121 +90,41 @@ export const Gallery = () => {
       >
         <a
           data-lg-size="1600-2400"
-          data-pinterest-text="Pin it2"
-          data-tweet-text="lightGallery slide  2"
           className="gallery__item"
-          data-src="https://wedding-fajri.s3.nevaobjects.id/nrimursjqvbyvbuzvsji.webp"
-          data-sub-html="<h4>Photo by - <a href=nrimursjqvbyvbuzvsji.webp' >Wedding </a></h4>"
+          data-src={IMAGES[0]}
+          data-sub-html="<h4>Photo by - <a href='#' >Wedding </a></h4>"
         >
-          <img
-            className="img-responsive"
-            src="https://wedding-fajri.s3.nevaobjects.id/nrimursjqvbyvbuzvsji.webp"
-          />
+          <img className="img-responsive" src={IMAGES[0]} loading="eager" decoding="sync" />
         </a>
-        <a
-          data-lg-size="1600-2400"
-          data-pinterest-text="Pin it3"
-          data-tweet-text="lightGallery slide  4"
-          className="gallery__item"
-          data-src="https://wedding-fajri.s3.nevaobjects.id/b6hgvbszjfghbwybfwpg.webp"
-        >
-          <img
-            className="img-responsive"
-            src="https://wedding-fajri.s3.nevaobjects.id/b6hgvbszjfghbwybfwpg.webp"
-          />
+
+        <a data-lg-size="1600-2400" className="gallery__item" data-src={IMAGES[1]}>
+          <img className="img-responsive" src={IMAGES[1]} loading="eager" decoding="sync" />
         </a>
-        <a
-          data-lg-size="1600-2398"
-          data-pinterest-text="Pin it3"
-          data-tweet-text="lightGallery slide  4"
-          className="gallery__item"
-          data-src="https://wedding-fajri.s3.nevaobjects.id/q0oezvlxhthdlgpyhzpy.webp"
-        >
-          <img
-            className="img-responsive"
-            src="https://wedding-fajri.s3.nevaobjects.id/q0oezvlxhthdlgpyhzpy.webp"
-          />
+
+        <a data-lg-size="1600-2398" className="gallery__item" data-src={IMAGES[2]}>
+          <img className="img-responsive" src={IMAGES[2]} loading="eager" decoding="sync" />
         </a>
-        <a
-          data-lg-size="1600-1067"
-          data-pinterest-text="Pin it3"
-          data-tweet-text="lightGallery slide  4"
-          className="gallery__item"
-          data-src="https://wedding-fajri.s3.nevaobjects.id/z16oluuzkmc3mwtqshco.webp"
-        >
-          <img
-            className="img-responsive"
-            src="https://wedding-fajri.s3.nevaobjects.id/z16oluuzkmc3mwtqshco.webp"
-          />
+
+        <a data-lg-size="1600-1067" className="gallery__item" data-src={IMAGES[3]}>
+          <img className="img-responsive" src={IMAGES[3]} loading="eager" decoding="sync" />
         </a>
-        <a
-          data-lg-size="1600-1067"
-          data-pinterest-text="Pin it3"
-          data-tweet-text="lightGallery slide  4"
-          className="gallery__item"
-          data-src="https://wedding-fajri.s3.nevaobjects.id/c1uv7cddikrv96eykn1v.webp"
-          // data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@flovayn' >Florian van Duyn</a></h4><p>Location - <a href='Bled, Slovenia'>Bled, Slovenia</a> </p>"
-        >
-          <img
-            className="img-responsive"
-            src="https://wedding-fajri.s3.nevaobjects.id/c1uv7cddikrv96eykn1v.webp"
-          />
+
+        <a data-lg-size="1600-1067" className="gallery__item" data-src={IMAGES[4]}>
+          <img className="img-responsive" src={IMAGES[4]} loading="eager" decoding="sync" />
         </a>
-        <a
-          data-lg-size="1600-2400"
-          data-pinterest-text="Pin it2"
-          data-tweet-text="lightGallery slide  2"
-          className="gallery__item"
-          data-src="https://wedding-fajri.s3.nevaobjects.id/eisztlp8fjvyxtzgpnyu.webp"
-          // data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@therawhunter' >Massimiliano Morosinotto </a></h4><p> Location - <a href='https://unsplash.com/s/photos/tre-cime-di-lavaredo%2C-italia'>Tre Cime di Lavaredo, Italia</a>This is the Way</p>"
-        >
-          <img
-            className="img-responsive"
-            src="https://wedding-fajri.s3.nevaobjects.id/eisztlp8fjvyxtzgpnyu.webp"
-          />
+
+        <a data-lg-size="1600-2400" className="gallery__item" data-src={IMAGES[5]}>
+          <img className="img-responsive" src={IMAGES[5]} loading="eager" decoding="sync" />
         </a>
-        <a
-          data-lg-size="1600-2400"
-          data-pinterest-text="Pin it3"
-          data-tweet-text="lightGallery slide  4"
-          className="gallery__item"
-          data-src="https://wedding-fajri.s3.nevaobjects.id/b6hgvbszjfghbwybfwpg.webp"
-          // data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@thesaboo' >Sascha Bosshard </a></h4><p> Location - <a href='https://unsplash.com/s/photos/pizol%2C-mels%2C-schweiz'>Pizol, Mels, Schweiz</a></p>"
-        >
-          <img
-            className="img-responsive"
-            src="https://wedding-fajri.s3.nevaobjects.id/b6hgvbszjfghbwybfwpg.webp"
-          />
+
+        <a data-lg-size="1600-2398" className="gallery__item" data-src={IMAGES[6]}>
+          <img className="img-responsive" src={IMAGES[6]} loading="eager" decoding="sync" />
         </a>
-        <a
-          data-lg-size="1600-2398"
-          data-pinterest-text="Pin it3"
-          data-tweet-text="lightGallery slide  4"
-          className="gallery__item"
-          data-src="https://wedding-fajri.s3.nevaobjects.id/hnjyhriidm2tx4rlga5s.webp"
-          // data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@yusufevli' >Yusuf Evli </a></h4><p> Foggy Road</p>"
-        >
-          <img
-            className="img-responsive"
-            src="https://wedding-fajri.s3.nevaobjects.id/hnjyhriidm2tx4rlga5s.webp"
-          />
-        </a>
-        <a
-          data-lg-size="1600-2398"
-          data-pinterest-text="Pin it3"
-          data-tweet-text="lightGallery slide  4"
-          className="gallery__item"
-          data-src="https://wedding-fajri.s3.nevaobjects.id/n3zfkaee9tlbcva0pchh.webp"
-          // data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@yusufevli' >Yusuf Evli </a></h4><p> Foggy Road</p>"
-        >
-          <img
-            className="img-responsive"
-            src="https://wedding-fajri.s3.nevaobjects.id/n3zfkaee9tlbcva0pchh.webp"
-          />
+
+        <a data-lg-size="1600-2398" className="gallery__item" data-src={IMAGES[7]}>
+          <img className="img-responsive" src={IMAGES[7]} loading="eager" decoding="sync" />
         </a>
       </LightGallery>
     </div>
   );
 };
-
-
